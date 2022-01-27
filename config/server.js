51bded82fc0c618b6070beff7437f3c845326219
Watsadonramai Wiroonthunyatorn <drysweet5683@gmail.com>
2022-01-28 01:54:27 +0700

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fddf616d750c1032818056cc0e15e7bb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b99bc1f4-478b-4af4-a210-7931f6e7ba9f'
+};

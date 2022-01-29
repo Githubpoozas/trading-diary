5374a65c65505085d7d7b389c42133f9a429d016
Watsadonramai Wiroonthunyatorn <drysweet5683@gmail.com>
2022-01-29 18:17:25 +0700

    add api publish data

---
+++ api/publish/controllers/publish.js
@@ -0,0 +1,35 @@
+"use strict";
+
+module.exports = {
+  async publish(ctx) {
+    const { name } = ctx.request.body;
+
+    try {
+      console.log("name", name);
+
+      const findRes = await strapi.query(name).find();
+      console.log("findRes", findRes);
+
+      const promises = findRes.map(async (item) => {
+        if (item.published_at) return;
+
+        const res = await strapi
+          .query(name)
+          .update({ id: item.id }, { published_at: new Date().toISOString() });
+
+        return res;
+      });
+
+      const publishRes = await Promise.all(promises);
+
+      ctx.response.status = 200;
+      ctx.response.body = publishRes;
+      return ctx.response;
+    } catch (error) {
+      console.log(`publish ${name} failed: ${error}`);
+      ctx.response.status = 400;
+      ctx.response.body = error;
+      return ctx.response;
+    }
+  },
+};

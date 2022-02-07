35be235e25c0d01ceabde4deb43bfc060b5b1749
Watsadonramai Wiroonthunyatorn <drysweet5683@gmail.com>
2022-02-08 04:01:39 +0700

    fix pre-line

--- plugins/trading-diary/admin/src/component/Table/index.js
+++ plugins/trading-diary/admin/src/component/Table/index.js
@@ -613,6 +613,7 @@ const OrderRow = ({
                 textAlign: "justify",
                 overflowWrap: "break-word",
                 width: "100%",
+                whiteSpace: "pre-line",
               }}
               color="blue"
             >
@@ -998,7 +999,10 @@ const TradeUpdateRow = memo(
                 onChange={handleChange}
               />
             ) : (
-              <Text bold="true" sx={{ wordBreak: "break-word" }}>
+              <Text
+                bold="true"
+                sx={{ wordBreak: "break-word", whiteSpace: "pre-line " }}
+              >
                 {comment}
               </Text>
             )}
@@ -1176,7 +1180,10 @@ const TradeRow = memo(
         </TableRow>
         <TableRow>
           <TableCell style={{ padding: "15px" }} colSpan={13}>
-            <Text bold="true" sx={{ wordBreak: "break-word" }}>
+            <Text
+              bold="true"
+              sx={{ wordBreak: "break-word", whiteSpace: "pre-line" }}
+            >
               {data.comment}
             </Text>
           </TableCell>
